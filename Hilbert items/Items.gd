d8ee898972f64d2b3f162d0906c1dbd9956604e3
AllanCao12 <99683096+AllanCao12@users.noreply.github.com>
2022-02-23 14:55:52 -0500

    Add files via upload

---
+++ Hilbert items/Items.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+class_name Items
+
+var cost = 0
+var resistance = 0
+var Connect1 = false
+var Connect2 = false
+var Volts = 200
---
+++ Hilbert items/Source/Global.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+var WorkCount = 0
+var Working = true
+var ShortCircuit = false
+var Volts = 200
+var TotalResistance = 0
+var Threeways = 0
